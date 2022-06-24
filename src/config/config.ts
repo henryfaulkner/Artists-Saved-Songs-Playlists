68ff7d400536b9cabdd97cd661a7bf6c2537d975
Henry Faulkner <hfaulkner27@gmail.com>
2022-06-24 13:32:24 -0400

    Heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8888;
 let domain: string;
-if(process.env.NODE_ENV === "prod") domain = "https://artists-saved-songs-playlists.glitch.me/";
+if(process.env.NODE_ENV === "prod") domain = "https://artists-saved-songs-playlists.herokuapp.com";
 else domain = "http://localhost:8888";
 
 export const config = {
