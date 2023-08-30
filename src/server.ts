6b3e19d2b76947cd6e9335e2d7cfb4f96cb164dc
Henry Faulkner <hfaulkner27@gmail.com>
2023-08-30 18:48:03 -0400

    goodbye google

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express(express.json());
+import { config } from "./config/config"
+
+let access_token;
+let refresh_token;
+
+app.use(require("./routes"))
+
+console.log(`Listening on port ${config.server.port}`)
+const server = app.listen(config.server.port)
+server.setTimeout(75*60*1000) //75min timeout for connections
